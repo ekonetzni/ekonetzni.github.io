335a975c0b2e0308fcfd26beaf365437229efcc2
Elliot Konetzni <elliot.konetzni@booking.com>
2020-01-31 15:11:14 +0100

    lots of new stuff

--- generate.js
+++ generate.js
@@ -1,8 +1,28 @@
 const fs = require('fs');
+
+// Gives us the timestamp at index 1
+// Gives us the name at index 2.
+const regex = /([0-9]{10}\.[0-9]{1,})-(.*)\..*\.[a-z0-9]*$/;
+
 const prefix = 'img/gen1i';
 
+const _date = timestamp => {
+  const d = new Date(timestamp * 1000);
+  return `${d.getFullYear()}`;
+};
+
+const images = fs.readdirSync(prefix).map(fileName => {
+  const tags = regex.exec(fileName);
+
+  return {
+    url: `${prefix}/${fileName}`,
+    title: tags[2],
+    date: _date(tags[1])
+  };
+});
+
 const mainfest = {
-  images: fs.readdirSync(prefix).map(fileName => `${prefix}/${fileName}`)
+  images
 };
 
 fs.writeFileSync('./manifest.json', JSON.stringify(mainfest), { flags: 'w' });

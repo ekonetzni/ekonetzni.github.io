2130fc0853bd8dbf457b169eebfdc3d1e7827b4d
Elliot Konetzni <elliot@konetzni.com>
2020-02-26 18:56:13 +0100

    Multi directory manifest generation

--- generate.js
+++ generate.js
@@ -4,22 +4,26 @@ const fs = require('fs');
 // Gives us the name at index 2.
 const regex = /([0-9]{10}\.[0-9]{1,})-(.*)\..*\.[a-z0-9]*$/;
 
-const prefix = 'img/gen1i';
+const prefixes = ['img/gen1i'];
 
 const _date = timestamp => {
   const d = new Date(timestamp * 1000);
   return `${d.getFullYear()}`;
 };
 
-const images = fs.readdirSync(prefix).map(fileName => {
-  const tags = regex.exec(fileName);
+const images = prefixes.reduce((acc, prefix) => {
+  return [
+    ...acc,
+    ...fs.readdirSync(prefix).map(fileName => {
+      const tags = regex.exec(fileName);
 
-  return {
-    url: `${prefix}/${fileName}`,
-    title: tags[2],
-    date: _date(tags[1])
-  };
-});
+      return {
+        url: `${prefix}/${fileName}`,
+        title: tags[2],
+        date: _date(tags[1])
+      };
+    })];
+}, []);
 
 const mainfest = {
   images
